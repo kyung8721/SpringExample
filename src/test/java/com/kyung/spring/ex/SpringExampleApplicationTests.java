6f6ddffdceeea9d1cc071b2f5689d3b48e2d315b
megait402 <.>
2024-08-05 20:35:16 +0900

    [학원]  - 최초 프로젝트 추가

---
+++ src/test/java/com/kyung/spring/ex/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyung.spring.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
