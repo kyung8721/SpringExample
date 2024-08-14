3e168dc9e625f8e3ba1c223a85a7ceacfce21571
kyung8721 <kyunbae8721@gmail.com>
2024-08-14 21:25:39 +0900

    [학원] - 24.08.14 수업  - spring에서 PK값 받아오는 법

--- src/main/java/com/kyung/spring/ex/mvc/repository/UserRepository.java
+++ src/main/java/com/kyung/spring/ex/mvc/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.kyung.spring.ex.mvc.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.kyung.spring.ex.mvc.domain.User;
 
@@ -9,4 +10,12 @@ public interface UserRepository {
 
 		// 가장 최근에 등록된 사용자 행 조회
 		public User selectLastUser();
+		
+		public int insertUser(
+				@Param("name") String name
+				, @Param("birthday") String birthday
+				, @Param("email") String email
+				, @Param("introduce") String introduce);
+		
+		public int insertUserByObject(User user);
 }
